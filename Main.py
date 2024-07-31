57db7297e656d69069fc9a27aafb7bd7b87758ea
Mazen Alhomsi <mazen.alhomsi@automotive-syergies.com>
2024-07-31 13:11:01 +0200

    commit

--- Main.py
+++ Main.py
@@ -8,11 +8,11 @@ coil_address = 0x0800
 
 # Coil values
 
-# OK = 0x0001
-# NOK = 0x0002
+OK = 0x0001
+NOK = 0x0002
 
-# Communication.write_coil(coil_address, OK)
-# time.sleep(5.0)
-# Communication.write_coil(coil_address, NOK)
-# time.sleep(5.0)
-# Communication.write_coil(coil_address, 0x0000)
+Communication.write_coil(coil_address, OK)
+time.sleep(5.0)
+Communication.write_coil(coil_address, NOK)
+time.sleep(5.0)
+Communication.write_coil(coil_address, 0x0000)
