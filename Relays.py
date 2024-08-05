26372c6cbbe9682907af9c777e38c520188087bf
Mazen Alhomsi <mazen.alhomsi@automotive-syergies.com>
2024-08-05 09:41:03 +0200

    commit

---
+++ Relays.py
@@ -0,0 +1,29 @@
+from gpiozero import LED
+from time import sleep
+
+# Setup the GPIO pins
+relay1 = LED(7)
+relay2 = LED(3)
+relay3 = LED(22)
+relay4 = LED(25)
+
+print("Relay testing!")
+
+while True:
+    relay1.on()
+    sleep(0.5)
+    relay1.off()
+    
+    relay2.on()
+    sleep(0.5)
+    relay2.off()
+    
+    relay3.on()
+    sleep(0.5)
+    relay3.off()
+    
+    relay4.on()
+    sleep(0.5)
+    relay4.off()
+    
+    sleep(0.5)
