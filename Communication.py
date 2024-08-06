2bce37721c7795a70c78ebbb1ce33a854fd17fac
Mazen Alhomsi <mazen.alhomsi@automotive-syergies.com>
2024-08-06 15:51:49 +0200

    comit

--- Communication.py
+++ Communication.py
@@ -33,6 +33,28 @@ def write_coil(address, value):
         print(f"General exception: {e}")
 
 
+# Coil address in decimal (0x0800 in hexadecimal is 2048 in decimal)
+coil_address = 0x0800
+
+# Coil values
+
+OK = 0x0001
+NOK = 0x0002
+
+# Communication.write_coil(coil_address, OK)
+# time.sleep(5.0)
+# Communication.write_coil(coil_address, NOK)
+# time.sleep(5.0)
+# Communication.write_coil(coil_address, 0x0000)
+
+
+
+
+
+
+
+
+
 
 
 # # Turn on the coil
--- Main.py
+++ Main.py
@@ -1,21 +1,14 @@
 import Communication;
-import time;
+import ip_forwarding;
+import mysql_re;
+import network_monitoring;
+import streamlit as st;
 
 
 
-# Coil address in decimal (0x0800 in hexadecimal is 2048 in decimal)
-coil_address = 0x0800
+# Eth1 Barcode Scanner Monitoring 
+network_monitoring.capture_packets("eth0");
 
-# Coil values
-
-OK = 0x0001
-NOK = 0x0002
-
-Communication.write_coil(coil_address, OK)
-time.sleep(5.0)
-Communication.write_coil(coil_address, NOK)
-time.sleep(5.0)
-Communication.write_coil(coil_address, 0x0000)
 
 
 
